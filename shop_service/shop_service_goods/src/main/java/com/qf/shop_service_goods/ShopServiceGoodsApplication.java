74df64b5af7a784b4876e4a5582c2d91a6fea43d
ldw <ldw@qq.com>
2019-04-09 17:51:53 +0800

    Initial commit

---
+++ shop_service/shop_service_goods/src/main/java/com/qf/shop_service_goods/ShopServiceGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.qf.shop_service_goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopServiceGoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopServiceGoodsApplication.class, args);
+	}
+
+}
